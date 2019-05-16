4e9f46ebf8f764a2ffb86774518b412c5eadb125
Forced Poet <poet.forced@gmail.com>
2019-05-16 21:17:19 +0530

    webcam demo

---
+++ _posts/2019-05-16-webcam-demo.markdown
@@ -0,0 +1,11 @@
+---
+layout: post
+title:  "Webcam Demo"
+date:   2019-05-16 20:06:23 +0530
+categories: jekyll update
+---
+
+
+{% include webcam.html %}
+<script src="{{base.url | prepend: site.url}}/asset/js/main.js"></script>
+
---
+++ asset/js/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const constraints = window.constraints = {
+  audio: false,
+  video: true
+};
+
+function handleSuccess(stream) {
+  const video = document.querySelector('video');
+  const videoTracks = stream.getVideoTracks();
+  window.stream = stream; 
+  video.srcObject = stream;
+}
+
+function handleError(error) {
+  errorMsg(`getUserMedia error: ${error.name}`, error);
+}
+
+function errorMsg(msg, error) {
+  const errorElement = document.querySelector('#errorMsg');
+  errorElement.innerHTML += `<p>${msg}</p>`;
+  if (typeof error !== 'undefined') {
+    console.error(error);
+  }
+}
+
+async function init(e) {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+    e.target.disabled = true;
+  } catch (e) {
+    handleError(e);
+  }
+}
+
+document.querySelector('#showVideo').addEventListener('click', e => init(e));
